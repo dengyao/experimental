49aa95b679a2225985c82d7342d71a4fa1e4b8f9
zhangpanyi@live.com <zhangpanyi@live.com>
2015-10-27 19:27:17 +0800

    update

--- eddy.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-
-#include "eddy/tcp_server.h"
-#include "eddy/message_filter.h"
-#include "eddy/tcp_session_handle.h"
-#include "eddy/io_service_thread_manager.h"
--- eddy/types.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-#include <thread>
-#include <cstdint>
-
-typedef std::thread::id		ThreadID;
-typedef uint32_t			TCPSessionID;
