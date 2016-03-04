441ef3b15b5acbfe94fbffe073a18dd6289779c6
zhangpanyi <zhangpanyi@live.com>
2016-03-04 16:35:19 +0800

    update

--- eddyserver.h
+++ eddyserver.h
@@ -5,6 +5,7 @@ namespace eddy
 {
 	class TCPClient;
 	class TCPServer;
+	class TaskQueue;
 	class TCPSessionHandle;
 	class SimpleMessageFilter;
 	class MessageFilterInterface;
@@ -13,6 +14,7 @@ namespace eddy
 
 #include "eddyserver/tcp_client.h"
 #include "eddyserver/tcp_server.h"
+#include "eddyserver/task_queue.h"
 #include "eddyserver/message_filter.h"
 #include "eddyserver/tcp_session_handle.h"
 #include "eddyserver/io_service_thread_manager.h"
