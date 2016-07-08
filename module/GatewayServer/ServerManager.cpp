4fd39b4097837cc8a84382f17bfaaa041461b57b
zhangpanyi@live.com <zhangpanyi@live.com>
2016-07-09 00:04:47 +0800

    update

---
+++ module/GatewayServer/ServerManager.cpp
@@ -0,0 +1,43 @@
+﻿#include "ServerManager.h"
+
+
+ServerManager::ServerManager()
+{
+
+}
+
+ServerManager::~ServerManager()
+{
+}
+
+// 服务器登录
+void ServerManager::OnServerLogin(int node_type, int child_id, google::protobuf::Message *message)
+{
+	node_lists_[node_type].child_lists[child_id].child_id = child_id;
+	node_lists_[node_type].child_lists[child_id].working = true;
+	node_lists_[node_type].child_lists[child_id].session_lists.push_back(seeiod);
+}
+
+// 服务器暂停服务
+void ServerManager::OnServerPauseWork(int node_type, int child_id, google::protobuf::Message *message)
+{
+	node_lists_[node_type].child_lists[child_id].working = false;
+}
+
+// 服务器继续服务器
+void ServerManager::OnServerContinueWor(int node_type, int child_id, google::protobuf::Message *message)
+{
+	node_lists_[node_type].child_lists[child_id].working = true;
+}
+
+// 转发服务器服务器消息
+void ServerManager::OnForwardServerMessage(int src_node_type, int src_child_id, int dst_node_type, int dst_child_id, google::protobuf::Message *message)
+{
+
+}
+
+// 广播服务器消息
+void ServerManager::OnBroadcastServerMessage(int src_node_type, int src_child_id, int dst_node_type, int dst_child_id, google::protobuf::Message *message)
+{
+
+}
---
+++ module/GatewayServer/ServerManager.h
@@ -0,0 +1,71 @@
+﻿#ifndef __SERVER_MANAGER_H__
+#define __SERVER_MANAGER_H__
+
+#include <map>
+#include <vector>
+
+namespace google
+{
+	namespace protobuf
+	{
+		class Message;
+	}
+}
+
+#define USE_DICT_STRUCTURE 1
+
+class SessionHandlePoint;
+
+struct ChildNode
+{
+	int child_id;
+	bool working;
+	std::vector<SessionHandlePoint*> session_lists;
+};
+
+#if (USE_DICT_STRUCTURE)
+struct ServerNode
+{
+	int node_type;
+	std::vector<ChildNode> child_lists;
+};
+
+typedef std::vector<ServerNode> ServerNodeContainer;
+#else
+struct ServerNode
+{
+	int node_type;
+	std::map<int, ChildNode> child_lists;
+};
+
+typedef std::map<int, ServerNode> ServerNodeContainer;
+#endif
+
+class ServerManager
+{
+public:
+	ServerManager();
+
+	~ServerManager();
+
+private:
+	// 服务器登录
+	void OnServerLogin(sads asdsa, google::protobuf::Message *message);
+
+	// 服务器暂停服务
+	void OnServerPauseWork(int node_type, int child_id, google::protobuf::Message *message);
+
+	// 服务器继续服务器
+	void OnServerContinueWor(int node_type, int child_id, google::protobuf::Message *message);
+
+	// 转发服务器服务器消息
+	void OnForwardServerMessage(int src_node_type, int src_child_id, int dst_node_type, int dst_child_id, google::protobuf::Message *message);
+
+	// 广播服务器消息
+	void OnBroadcastServerMessage(int src_node_type, int src_child_id, int dst_node_type, int dst_child_id, google::protobuf::Message *message);
+
+private:
+	ServerNodeContainer node_lists_;
+};
+
+#endif
