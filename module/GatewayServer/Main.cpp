2ebb4eb870b7083f3c5184abf248c3038f5c41a9
zhangpanyi@live.com <zhangpanyi@live.com>
2016-07-12 22:11:04 +0800

    update

--- module/GatewayServer/Main.cpp
+++ module/GatewayServer/Main.cpp
@@ -1,8 +1,14 @@
 ﻿#include <iostream>
+#include "SessionHandle.h"
 #include "GatewayManager.h"
-
 
 int main(int argc, char *argv[])
 {
+	asio::ip::tcp::endpoint endpoint(asio::ip::address_v4(), 4235);
+	eddy::IOServiceThreadManager threads(std::thread::hardware_concurrency());
+	GatewayManager manager(threads);
+	eddy::TCPServer server(endpoint, threads, std::bind(CreateSessionHandle, std::ref(manager)), CreateMessageFilter);
+	threads.Run();
+
 	return 0;
 }
